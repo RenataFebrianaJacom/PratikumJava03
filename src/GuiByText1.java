14a3ee35e9aae9e7542ebb33e6475c850390d9b9
RenataFebrianaJacom <renatafebrianajacom@gmal.com>
2021-06-13 13:17:28 +0800

    menambahkan setlocation,settitle,dan setdefault

--- src/GuiByText1.java
+++ src/GuiByText1.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 public class GuiByText1 extends JFrame {
     GuiByText1(){
         setSize(640, 480);
+        setLocationRelativeTo(null);
+        setTitle("GUI By Text");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
