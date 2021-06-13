fb833529d2799a6cbd100edd24e6a0a8fe1e926c
RenataFebrianaJacom <renatafebrianajacom@gmal.com>
2021-06-13 13:14:26 +0800

    membuat GuiByText()

---
+++ src/GuiByText1.java
@@ -0,0 +1,13 @@
+
+import javax.swing.*;
+
+public class GuiByText1 extends JFrame {
+    GuiByText1(){
+        setSize(640, 480);
+    }
+
+    public static void main(String[] args) {
+        GuiByText guiByText = new GuiByText();
+        guiByText.setVisible(true);
+    }
+}
